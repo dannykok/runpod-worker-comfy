613f350581120a8dfbd01e4cae18ecd02c8bbffc
dannykok <dannykok1@gmail.com>
2024-08-12 05:43:44 +0800

    remove s3.py and move into rp_handler.py

--- src/rp_handler.py
+++ src/rp_handler.py
@@ -10,7 +10,9 @@ import base64
 from io import BytesIO
 from requests_toolbelt import MultipartEncoder
 from requests_toolbelt.multipart.encoder import FileFromURLWrapper
-from src.s3 import upload_files_to_s3
+from boto3 import session
+from botocore.config import Config
+
 
 # Time to wait between API check attempts in milliseconds
 COMFY_API_AVAILABLE_INTERVAL_MS = 50
@@ -315,6 +317,50 @@ def is_an_output_file(file_dict: dict):
         bool: True if the file is an output file, False otherwise
     """
     return "filename" in file_dict and "type" in file_dict and file_dict["type"] == "output"
+
+
+def upload_files_to_s3(job_id: str,
+                       file_list: list,
+                       bucket_name: str,
+                       endpoint_url: str,
+                       access_key: str,
+                       secret_key: str,
+                       ):
+    '''
+    Uploads files to s3 bucket storage.
+    '''
+    config = Config(
+        signature_version='s3v4',
+        retries={
+            'max_attempts': 3,
+            'mode': 'standard'
+        }
+    )
+
+    client_session = session.Session()
+
+    client = client_session.client(
+        's3',
+        endpoint_url=endpoint_url.rstrip('/'),
+        aws_access_key_id=access_key,
+        aws_secret_access_key=secret_key,
+        config=config)
+
+    bucket_urls = []
+
+    for _file in file_list:
+        with open(_file, 'rb') as file_data:
+            file_name = _file.split('/')[-1]
+            object_key = f"{job_id}/{file_name}"
+            try:
+                res = client.upload_file(
+                    _file, bucket_name, object_key)
+                bucket_urls.append(f"{endpoint_url}/{object_key}")
+            except Exception as e:
+                print(f"Error uploading file: {e}")
+                raise e
+
+    return bucket_urls
 
 
 def process_output_images(outputs, job_id, job_output_def=None):
